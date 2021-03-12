65d89c4aefbd519bc476d369ff6011ee28cede5b
Sebastian Korfmann <sebastian@korfmann.net>
2021-03-12 21:20:10 +0100

    Initial import

---
+++ packages/jest-lambda/lib/index.ts
@@ -0,0 +1,161 @@
+import throat from 'throat';
+import Worker from 'jest-worker';
+
+class CancelRun extends Error {
+  constructor(message?: string) {
+    super(message);
+    this.name = 'CancelRun';
+  }
+}
+
+const createRunner = (runPath: string, { getExtraOptions }: any = {}) => {
+  class BaseTestRunner {
+    private _globalConfig: any;
+
+    constructor(globalConfig: any) {
+      this._globalConfig = globalConfig;
+    }
+
+    runTests(tests: any, watcher: any, onStart: any, onResult: any, onFailure: any, options: any) {
+      return options.serial
+        ? this._createInBandTestRun(
+            tests,
+            watcher,
+            onStart,
+            onResult,
+            onFailure,
+            options,
+          )
+        : this._createParallelTestRun(
+            tests,
+            watcher,
+            onStart,
+            onResult,
+            onFailure,
+            options,
+          );
+    }
+
+    _createInBandTestRun(
+      tests: any,
+      watcher: any,
+      onStart: any,
+      onResult: any,
+      onFailure: any,
+      options: any,
+    ) {
+      const mutex = throat(1);
+      return tests.reduce(
+        (promise: any, test: any) =>
+          mutex(() =>
+            promise
+              .then(() => {
+                if (watcher.isInterrupted()) {
+                  throw new CancelRun();
+                }
+
+                return onStart(test).then(() => {
+                  // eslint-disable-next-line import/no-dynamic-require, global-require
+                  const runner = require(runPath);
+                  const baseOptions = {
+                    config: test.context.config,
+                    globalConfig: this._globalConfig,
+                    testPath: test.path,
+                    rawModuleMap: watcher.isWatchMode()
+                      ? test.context.moduleMap.getRawModuleMap()
+                      : null,
+                    options,
+                    extraOptions: getExtraOptions ? getExtraOptions() : {},
+                  };
+
+                  if (typeof runner.default === 'function') {
+                    return runner.default(baseOptions);
+                  }
+
+                  return runner(baseOptions);
+                });
+              })
+              .then((result: any) => onResult(test, result))
+              .catch((err: any) => onFailure(test, err)),
+          ),
+        Promise.resolve(),
+      );
+    }
+
+    _createParallelTestRun(
+      tests: any,
+      watcher: any,
+      onStart: any,
+      onResult: any,
+      onFailure: any,
+      options: any,
+    ) {
+      const worker = new Worker(runPath, {
+        exposedMethods: ['default'],
+        numWorkers: this._globalConfig.maxWorkers,
+        forkOptions: { stdio: 'inherit' },
+      });
+
+      const mutex = throat(this._globalConfig.maxWorkers);
+
+      const runTestInWorker = (test: any) =>
+        mutex(() => {
+          if (watcher.isInterrupted()) {
+            throw new CancelRun();
+          }
+
+          return onStart(test).then(() => {
+            const baseOptions = {
+              config: test.context.config,
+              globalConfig: this._globalConfig,
+              testPath: test.path,
+              rawModuleMap: watcher.isWatchMode()
+                ? test.context.moduleMap.getRawModuleMap()
+                : null,
+              options,
+              extraOptions: getExtraOptions ? getExtraOptions() : {},
+            };
+
+            return (worker as any).default(baseOptions);
+          });
+        });
+
+      const onError = (err: any, test: any) => {
+        return onFailure(test, err).then(() => {
+          if (err.type === 'ProcessTerminatedError') {
+            // eslint-disable-next-line no-console
+            console.error(
+              'A worker process has quit unexpectedly! ' +
+                'Most likely this is an initialization error.',
+            );
+            process.exit(1);
+          }
+        });
+      };
+
+      const onInterrupt = new Promise((_, reject) => {
+        watcher.on('change', (state: any) => {
+          if (state.interrupted) {
+            reject(new CancelRun());
+          }
+        });
+      });
+
+      const runAllTests = Promise.all(
+        tests.map((test: any) =>
+          runTestInWorker(test)
+            .then(testResult => onResult(test, testResult))
+            .catch(error => onError(error, test)),
+        ),
+      );
+
+      const cleanup = () => worker.end();
+
+      return Promise.race([runAllTests, onInterrupt]).then(cleanup, cleanup);
+    }
+  }
+
+  return BaseTestRunner;
+};
+
+export default createRunner;
